72cf075b519962c60a5bfa96afb8d2c73e053624
Vinod Mahalingam <vmahalingam@godaddy.com>
2024-02-26 01:16:25 -0800

    PAY-3511 Make clojure 1.6.0 the default and add poynt nexus repos

--- project.clj
+++ project.clj
@@ -1,18 +1,20 @@
-(defproject baizen "0.6.0"
+(defproject baizen "0.6.0-SNAPSHOT"
   :description "BAI file format parser"
   :url "https://github.com/testdouble/baizen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/core.match "0.2.2"]]
   :profiles {:dev {:resource-paths ["test-resources"]}
-             :1.5 {:resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/clojure "1.5.1" :scope "provided"]]}
              :1.6 {:resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]]}
+                   :dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:resource-paths ["test-resources"]
                    :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]}
              :1.8 {:resource-paths ["test-resources"]
                    :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]]}}
-  :aliases {"test-all-profiles" ["with-profile" "dev:1.5:1.6:1.7:1.8" "test"]})
+  :aliases {"test-all-profiles" ["with-profile" "dev:1.6:1.7:1.8" "test"]}
+  :deploy-repositories [["snapshots" {:url "https://nexus.poynt.com/content/repositories/snapshots"}]
+                        ["releases" {:url "https://nexus.poynt.com/content/repositories/releases"}]]
+  
+  :repositories [["poynt" "https://nexus.poynt.com/content/groups/public"]])
